db785123a1c91aab4c49214b2e378c5b4597d3c5
Viraj <virajlasithazeero@gmail.com>
2020-11-07 14:34:16 +0530

    Update Dashboard.php

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,10 +1,27 @@
 <?php
-class Dashboard extends CI_Controller 
+class Dashboard extends Admin_Controller 
 {
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->not_logged_in();
+		// $this->data['page_title'] = 'Dashboard - KNC';
+		// $this->load->model('model_dashboard');
+	}
+
 	public function index()
 	{
-		$this->load->view('partials/header');
-		$this->load->view('dashboard');
+		$this->data['page_title'] = 'Dashboard';
+
+		$this->load->view('partials/header', $this->data);
+		$this->load->view('Dashboard');
+		$this->load->view('dashboard/widgetSet_01');
 		$this->load->view('partials/footer');
+		// $this->render_template('Dashboard');
+	}
+
+	public function getDashboardData(){
+
 	}
 }
