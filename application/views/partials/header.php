3d1ed724aa7741fbbe371d50e0dec081f1af3e8c
Viraj <virajlasithazeero@gmail.com>
2020-11-07 14:42:17 +0530

    2020-11-07

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -8,7 +8,16 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>K N C | Business Management System</title>
+	<?php
+	if (isset($page_title)) { ?>
+		<title><?= $page_title ?> | K N C</title>
+	<?php
+	} else { ?>
+		<!-- <title>K N C | Business Management System</title> -->
+		<title><?= $this->uri->segment(1) ?> | K N C</title>
+	<?php
+	}
+	?>
 	<!-- Tell the browser to be responsive to screen width -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -177,7 +186,7 @@
 				<span class="brand-text font-weight-light">AdminLTE 3</span>
 			</a> -->
 
- 
+
 			<a href="#" class="brand-link">
 				<div class="user-panel pb-3 d-flex">
 					<!-- <div class="image">
@@ -225,13 +234,17 @@
 						</li>
 
 						<li class="nav-item">
-							<a href="<?= base_url('dashboard') ?>" class="nav-link <?php if ($this->uri->segment(1) == "dashboard") { echo 'active'; } ?>">
+							<a href="<?= base_url('dashboard') ?>" class="nav-link <?php if ($this->uri->segment(1) == "dashboard") {
+																						echo 'active';
+																					} ?>">
 								<i class="fas fa-tachometer-alt"></i>
 								<p>&nbsp;&nbsp;&nbsp;Dashboard</p>
 							</a>
 						</li>
 
-						<li class="nav-item has-treeview <?php if ($this->uri->segment(1) == "User" || $this->uri->segment(2) == "UserGroup" || $this->uri->segment(2) == "MeasureUnit") { echo 'menu-open'; } ?>">
+						<li class="nav-item has-treeview <?php if ($this->uri->segment(1) == "User" || $this->uri->segment(2) == "UserGroup" || $this->uri->segment(2) == "MeasureUnit") {
+																echo 'menu-open';
+															} ?>">
 							<a href="#" class="nav-link">
 								<i class="fas fa-tools"></i>
 								<p>&nbsp;&nbsp;&nbsp;Utilities
@@ -240,19 +253,25 @@
 							</a>
 							<ul class="nav nav-treeview">
 								<li class="nav-item">
-									<a href="<?php echo base_url('User/index') ?>" class="nav-link <?php if ($this->uri->segment(1) == "User") { echo 'active'; } ?>">
+									<a href="<?php echo base_url('User/index') ?>" class="nav-link <?php if ($this->uri->segment(1) == "User") {
+																										echo 'active';
+																									} ?>">
 										<i class="fas fa-user"></i>
 										<p>&nbsp;&nbsp;&nbsp;User</p>
 									</a>
 								</li>
 								<li class="nav-item">
-									<a href="<?= base_url('Utilities/UserGroup') ?>" class="nav-link <?php if ($this->uri->segment(2) == "UserGroup") { echo 'active'; } ?>">
+									<a href="<?= base_url('Utilities/UserGroup') ?>" class="nav-link <?php if ($this->uri->segment(2) == "UserGroup") {
+																											echo 'active';
+																										} ?>">
 										<i class="fas fa-users"></i>
 										<p>&nbsp;&nbsp;User Group</p>
 									</a>
 								</li>
 								<li class="nav-item">
-									<a href="<?= base_url('Utilities/MeasureUnit') ?>" class="nav-link <?php if ($this->uri->segment(2) == "MeasureUnit") { echo 'active'; } ?>">
+									<a href="<?= base_url('Utilities/MeasureUnit') ?>" class="nav-link <?php if ($this->uri->segment(2) == "MeasureUnit") {
+																											echo 'active';
+																										} ?>">
 										<i class="fas fa-balance-scale-right"></i>
 										<p>&nbsp;&nbsp;Measure Unit</p>
 									</a>
@@ -260,33 +279,43 @@
 							</ul>
 						</li>
 						<li class="nav-item">
-							<a href="<?php echo base_url('Customer/index') ?>" class="nav-link <?php if ($this->uri->segment(1) == "Customer") { echo 'active'; } ?>">
+							<a href="<?php echo base_url('Customer/index') ?>" class="nav-link <?php if ($this->uri->segment(1) == "Customer") {
+																									echo 'active';
+																								} ?>">
 								<i class="fa fa-users" aria-hidden="true"></i>
 								<p>&nbsp;&nbsp;Customer</p>
 							</a>
 						</li>
 
 						<li class="nav-item">
-							<a href="<?php echo base_url('Supplier/index') ?>" class="nav-link <?php if ($this->uri->segment(1) == "Supplier") { echo 'active'; } ?>">
+							<a href="<?php echo base_url('Supplier/index') ?>" class="nav-link <?php if ($this->uri->segment(1) == "Supplier") {
+																									echo 'active';
+																								} ?>">
 								<i class="fa fa-truck" aria-hidden="true"></i>
 								<p>&nbsp;&nbsp;Supplier</p>
 							</a>
 						</li>
 
 						<li class="nav-item">
-							<a href="<?php echo base_url('Branch/index') ?>" class="nav-link <?php if ($this->uri->segment(1) == "Branch") { echo 'active'; } ?>">
+							<a href="<?php echo base_url('Branch/index') ?>" class="nav-link <?php if ($this->uri->segment(1) == "Branch") {
+																									echo 'active';
+																								} ?>">
 								<i class="fas fa-store"></i>
 								<p>&nbsp;&nbsp;Branch</p>
 							</a>
 						</li>
 
 						<li class="nav-item">
-							<a href="<?php echo base_url('Item/index') ?>" class="nav-link <?php if ($this->uri->segment(1) == "Item") { echo 'active'; } ?>">
+							<a href="<?php echo base_url('Item/index') ?>" class="nav-link <?php if ($this->uri->segment(1) == "Item") {
+																								echo 'active';
+																							} ?>">
 								<i class="fa fa-industry" aria-hidden="true"></i>
 								<p>&nbsp;&nbsp;&nbsp;Item</p>
 							</a>
 						</li>
-						<li class="nav-item has-treeview <?php if ($this->uri->segment(2) == "RequestItem" || $this->uri->segment(2) == "ViewRequest") { echo 'menu-open'; } ?>">
+						<li class="nav-item has-treeview <?php if ($this->uri->segment(2) == "RequestItem" || $this->uri->segment(2) == "ViewRequest") {
+																echo 'menu-open';
+															} ?>">
 							<a href="#" class="nav-link">
 								<i class="fas fa-layer-group"></i>
 								<p>&nbsp;&nbsp;&nbsp;Request
@@ -295,13 +324,17 @@
 							</a>
 							<ul class="nav nav-treeview">
 								<li class="nav-item">
-									<a href="<?php echo base_url('Request/RequestItem') ?>" class="nav-link <?php if ($this->uri->segment(2) == "RequestItem") { echo 'active'; } ?>">
+									<a href="<?php echo base_url('Request/RequestItem') ?>" class="nav-link <?php if ($this->uri->segment(2) == "RequestItem") {
+																												echo 'active';
+																											} ?>">
 										<i class="fas fa-cart-plus"></i>
 										<p>&nbsp;&nbsp;Request Item</p>
 									</a>
 								</li>
 								<li class="nav-item">
-									<a href="<?php echo base_url('Request/ViewRequest') ?>" class="nav-link <?php if ($this->uri->segment(2) == "ViewRequest") { echo 'active'; } ?>">
+									<a href="<?php echo base_url('Request/ViewRequest') ?>" class="nav-link <?php if ($this->uri->segment(2) == "ViewRequest") {
+																												echo 'active';
+																											} ?>">
 										<i class="fas fa-search"></i>
 										<p>&nbsp;&nbsp;View Request</p>
 									</a>
@@ -309,7 +342,9 @@
 							</ul>
 						</li>
 
-						<li class="nav-item has-treeview <?php if ($this->uri->segment(2) == "CreateGRN" || $this->uri->segment(2) == "ViewGRN") { echo 'menu-open'; } ?>">
+						<li class="nav-item has-treeview <?php if ($this->uri->segment(2) == "CreateGRN" || $this->uri->segment(2) == "ViewGRN") {
+																echo 'menu-open';
+															} ?>">
 							<a href="#" class="nav-link">
 								<i class="fas fa-layer-group"></i>
 								<p>&nbsp;&nbsp;&nbsp;Stock
@@ -318,13 +353,17 @@
 							</a>
 							<ul class="nav nav-treeview">
 								<li class="nav-item">
-									<a href="<?php echo base_url('GRN/CreateGRN') ?>" class="nav-link <?php if ($this->uri->segment(2) == "CreateGRN") { echo 'active'; } ?>">
+									<a href="<?php echo base_url('GRN/CreateGRN') ?>" class="nav-link <?php if ($this->uri->segment(2) == "CreateGRN") {
+																											echo 'active';
+																										} ?>">
 										<i class="fas fa-cart-plus"></i>
 										<p>&nbsp;&nbsp;Create GRN</p>
 									</a>
 								</li>
 								<li class="nav-item">
-									<a href="<?php echo base_url('GRN/ViewGRN') ?>" class="nav-link <?php if ($this->uri->segment(2) == "ViewGRN") { echo 'active'; } ?>">
+									<a href="<?php echo base_url('GRN/ViewGRN') ?>" class="nav-link <?php if ($this->uri->segment(2) == "ViewGRN") {
+																										echo 'active';
+																									} ?>">
 										<i class="fas fa-search"></i>
 										<p>&nbsp;&nbsp;View GRN</p>
 									</a>
