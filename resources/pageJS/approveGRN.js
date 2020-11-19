44bdb8f485747867417a6aa19c05bffdc47ca02c
Viraj <virajlasithazeero@gmail.com>
2020-11-19 21:53:11 +0530

    Update approveGRN.js

--- resources/pageJS/approveGRN.js
+++ resources/pageJS/approveGRN.js
@@ -33,3 +33,28 @@ function approveGRN(GRNHeaderID) {
         });
     }, this);
 }
+
+function rejectGRN(GRNHeaderID){
+    arcadiaConfirmAlert("You want to be able to reject this !", function (button) {
+
+        $.ajax({
+            async: true,
+            url: base_url + 'GRN/RejectGRN',
+            type: 'post',
+            data: {
+                intGRNHeaderID: GRNHeaderID
+            },
+            dataType: 'json',
+            success: function (response) {
+                if (response.success == true) {
+                    arcadiaSuccessMessage("Rejected !", "GRN/ViewGRN");
+                } else {
+                    toastr["error"](response.messages);
+                }
+            },
+            error: function (request, status, error) {
+                arcadiaErrorMessage(error);
+            }
+        });
+    }, this);
+}
