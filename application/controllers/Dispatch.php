e631ad554b01c1ad577c6504a60fe4694447ac37
Viraj <virajlasithazeero@gmail.com>
2020-11-27 02:39:12 +0530

    Update Dispatch.php

--- application/controllers/Dispatch.php
+++ application/controllers/Dispatch.php
@@ -1,22 +1,40 @@
 <?php
 class Dispatch extends Admin_Controller
 {
-	public function __construct()
-	{
-		parent::__construct();
+    public function __construct()
+    {
+        parent::__construct();
         $this->not_logged_in();
         $this->load->model('model_item');
-        $this->load->model('model_grn');
+        $this->load->model('model_dispatch');
     }
-    
-    public function CreateDispatch(){
+
+    //-----------------------------------
+    // Create Dispatch
+    //-----------------------------------
+
+    public function CreateDispatch()
+    {
         if (!$this->isAdmin) {
-            if (!in_array('createDispatch',$this->permission)) {
+            if (!in_array('createDispatch', $this->permission)) {
                 redirect('dashboard', 'refresh');
             }
         }
         $item_data = $this->model_item->getOnlyRawItemData();
         $this->data['item_data'] = $item_data;
         $this->render_template('Dispatch/createDispatch', 'Create Dispatch',  $this->data);
+    }
+
+    public function SaveDispatch()
+    {
+        if (!$this->isAdmin) {
+            if (!in_array('createDispatch', $this->permission)) {
+                redirect('dashboard', 'refresh');
+            }
+        }
+
+        $response = $this->model_dispatch->saveDispatch();
+
+        echo json_encode($response);
     }
 }
