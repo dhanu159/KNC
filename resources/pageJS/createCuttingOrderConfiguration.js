1162e68d71864c126f1441f20dbfc6005277220b
Ramodk <42975445+Ramodk@users.noreply.github.com>
2020-11-27 02:40:11 +0530

    Update createCuttingOrderConfiguration.js

--- resources/pageJS/createCuttingOrderConfiguration.js
+++ resources/pageJS/createCuttingOrderConfiguration.js
@@ -1,12 +1,6 @@
 $(document).ready(function() {
 
 
-
-    // $('#itemTable tbody tr').each(function() {
-    //     var value = $(this).closest("tr").find('.itemID').val();
-    //     $("#cmbItem option[value=" + value + "]").remove();
-    // });
-
     // on first focus (bubbles up to document), open the menu
     $(document).on('keyup', 'input[type=search]', function(e) {
         $("li").attr('aria-selected', false);
@@ -14,158 +8,146 @@ $(document).ready(function() {
 
 
     $('#cmbItem').on('select2:select', function(e) {
+        remove();
         fetchCuttingConfigData();
-        // findAlreadItemAdded();
-        // $('#cmbCuttingOrder').focus();
+        cmbCuttingOrder();
+        findAlreadItemAdded();
+        remove();
 
+    });
+
+    $("#cmbCuttingOrder").next(".select2").mouseenter(function() {
+        findAlreadItemAdded();
     });
 
     $("#btnAddToGrid").click(function() {
-        AddToGrid();
+
+        if ($("#cmbCuttingOrder :selected").val() == 0) {
+            toastr["error"]("Please select a Cutting Order !");
+        } else if (IsAlreadyIncluded() == false) {
+            ConfigCuttingOrder();
+        }
+        // }
     });
+
     //Bind keypress event to textbox
     $('.add-item').keypress(function(event) {
         var keycode = (event.keyCode ? event.keyCode : event.which);
         if (keycode == '13') {
             $('#btnAddToGrid').focus();
-            AddToGrid();
+            if (IsAlreadyIncluded() == false) {
+                ConfigCuttingOrder();
+            }
         }
-
         event.stopPropagation();
     });
 
-
-
-    function cmbItemDisable() {
-        var rowCount = $('#itemTable tr').length;
-        if (rowCount > 1) {
-            document.getElementById("cmbItem").disabled = true;
-        }
-        // if (rowCount == 1) {
-        //     document.getElementById("cmbItem").disabled = false;
-        // }
-    }
-
-    function cmbItemEnable() {
-        var rowCount = $('#itemTable tr').length;
-        if (rowCount == 1) {
-            document.getElementById("cmbItem").disabled = false;
-        }
-    }
-
-
     remove();
-
-
-    var row_id = 1;
-
-    function AddToGrid() {
-        cmbItemDisable();
-        if ($('#cmbItem').val() == null) {
-            toastr["error"]("Please select a Item !");
-            $("#cmbItem").focus();
-        } else if ($("input[name=cmbCuttingOrder]").val()) {
-            toastr["error"]("Please select a Cutting Order !");
-        } else {
-            if ($("#cmbCuttingOrder option:selected").val() > 0) {
-                var itemID = $("#cmbItem option:selected").val();
-                var cuttingorderID = $("#cmbCuttingOrder option:selected").val();
-                var cuttingorder = $("#cmbCuttingOrder option:selected").text();
-
-
-                $(".first-tr").after('<tr>' +
-                    '<td hidden>' +
-                    '<input type="text" class="form-control itemID disable-typing" name="itemID[]" id="itemID_' + row_id + '" value="' + itemID + '" readonly>' +
-                    '</td>' +
-                    '<td hidden>' +
-                    '<input type="text" class="form-control cuttingorderID disable-typing" name="cuttingorderID[]" id="cuttingorderID_' + row_id + '" value="' + cuttingorderID + '" readonly>' +
-                    '</td>' +
-                    '<td>' +
-                    '<input type="text" class="form-control cuttingorder disable-typing" name="cuttingorder[]" id="cuttingorder_' + row_id + '" value="' + cuttingorder + '" readonly>' +
-                    '</td>' +
-                    '<td class="static">' +
-                    '<span class="button red center-items"><i class="fas fa-times"></i></span>' +
-                    '</td>' +
-                    '</tr>');
-
-                row_id++;
-                remove();
-
-                $("#cmbCuttingOrder :selected").remove();
-
-                $("input[name=cmbCuttingOrder]").val("");
-                CalculateItemCount();
-
-                $("#cmbCuttingOrder").focus();
-                $("li").attr('aria-selected', false);
-                $("#cmbCuttingOrder").focus();
-
-
-            } else {
-                toastr["error"]("Please select valid item !");
-                $("#cmbCuttingOrder").focus();
-                $("li").attr('aria-selected', false);
-            }
-        }
-    }
-
-
-
-
-    $('#btnSubmit').click(function() {
-
-        if ($('#itemTable tr').length == 2) {
-            toastr["error"]("Please choose the receive items !");
-            $("#cmbCuttingOrder").focus();
-        } else {
-            arcadiaConfirmAlert("You want to be able to save this !", function(button) {
-
-                var form = $("#createCuttingOrderConfiguration");
-
-                $.ajax({
-                    type: form.attr('method'),
-                    url: form.attr('action'),
-                    data: form.serialize(),
-                    dataType: 'json',
-                    async: true,
-                    success: function(response) {
-                        if (response.success == true) {
-                            arcadiaSuccessMessage(true);
-                        } else {
-
-                            if (response.messages instanceof Object) {
-                                $.each(response.messages, function(index, value) {
-                                    var id = $("#" + index);
-
-                                    id.closest('.form-group')
-                                        .removeClass('has-error')
-                                        .removeClass('has-success')
-                                        .addClass(value.length > 0 ? 'has-error' : 'has-success');
-
-                                    id.after(value);
-
-                                });
-                            } else {
-                                toastr["error"](response.messages);
-
-                                arcadiaErrorMessage(response.messages);
-                            }
-                        }
-
-                    }
-                });
-            }, this);
-        }
-
-    });
-
-    $("#createCuttingOrderConfiguration").unbind('submit').on('submit', function(e) {});
 
 });
 
+function AddToGrid() {
+    var row_id = 1;
+
+    if ($('#cmbItem').val() == null) {
+        toastr["error"]("Please select a Item !");
+        $("#cmbItem").focus();
+    } else if ($("input[name=cmbCuttingOrder]").val()) {
+        toastr["error"]("Please select a Cutting Order !");
+    } else {
+        if ($("#cmbCuttingOrder option:selected").val() > 0) {
+            var itemID = $("#cmbItem option:selected").val();
+            var cuttingorderID = $("#cmbCuttingOrder option:selected").val();
+            var cuttingorder = $("#cmbCuttingOrder option:selected").text();
+
+            $(".first-tr").after('<tr>' +
+                '<td hidden>' +
+                '<input type="text" class="form-control itemID disable-typing" name="itemID[]" id="itemID_' + row_id + '" value="' + itemID + '" readonly>' +
+                '</td>' +
+                '<td hidden>' +
+                '<input type="text" class="form-control cuttingorderID disable-typing" name="cuttingorderID[]" id="cuttingorderID_' + row_id + '" value="' + cuttingorderID + '" readonly>' +
+                '</td>' +
+                '<td>' +
+                '<input type="text" class="form-control cuttingorder disable-typing" name="cuttingorder[]" id="cuttingorder_' + row_id + '" value="' + cuttingorder + '" readonly>' +
+                '</td>' +
+                '<td class="static">' +
+                '<span class="button red center-items"><i class="fas fa-times"></i></span>' +
+                '</td>' +
+                '</tr>');
+
+            row_id++;
+            remove();
+
+            $("#cmbCuttingOrder :selected").remove();
+
+            $("input[name=cmbCuttingOrder]").val("");
+            CalculateItemCount();
+
+            $("#cmbCuttingOrder").focus();
+            $("li").attr('aria-selected', false);
+            $("#cmbCuttingOrder").focus();
+
+
+        } else {
+            toastr["error"]("Please select valid item !");
+            $("#cmbCuttingOrder").focus();
+            $("li").attr('aria-selected', false);
+        }
+    }
+}
+
+function ConfigCuttingOrder() {
+    //confim message box
+    //ajax save
+    //save success add grid call
+
+    arcadiaConfirmAlert("You want to be able to save this !", function(button) {
+
+        var ItemID = $("#cmbItem :selected").val();
+        var CuttingOrderHeaderID = $("#cmbCuttingOrder :selected").val();
+
+        $.ajax({
+            type: 'post',
+            url: base_url + 'Utilities/SaveConfigCuttingOrderUsingFunction/' + ItemID + '/' + CuttingOrderHeaderID,
+            dataType: 'json',
+            async: true,
+            success: function(response) {
+                if (response.success == true) {
+                    // arcadiaSuccessMessage(true);
+                    toastr["success"](response.messages);
+                    AddToGrid();
+
+                } else {
+
+                    if (response.messages instanceof Object) {
+                        $.each(response.messages, function(index, value) {
+                            var id = $("#" + index);
+
+                            id.closest('.form-group')
+                                .removeClass('has-error')
+                                .removeClass('has-success')
+                                .addClass(value.length > 0 ? 'has-error' : 'has-success');
+
+                            id.after(value);
+
+                        });
+                    } else {
+                        toastr["error"](response.messages);
+
+                        // arcadiaErrorMessage(response.messages);
+                    }
+                }
+
+            }
+        });
+    }, this);
+}
+
+
+
 function fetchCuttingConfigData() {
-    debugger;
     var value = $("#cmbItem :selected").val();
+
     $.ajax({
         url: 'fetchCuttingConfigDataByItemID/' + value,
         type: 'post',
@@ -175,8 +157,11 @@ function fetchCuttingConfigData() {
             var html = '';
             var row_id = 1;
             for (var i = 0; i < response.length; i++) {
-                debugger;
+                var itemID = $("#cmbItem option:selected").val();
                 html += '<tr>' +
+                    '<td hidden>' +
+                    '<input type="text" class="form-control itemID disable-typing" name="itemID[]" id="itemID_' + row_id + '" value="' + itemID + '" readonly>' +
+                    '</td>' +
                     '<td hidden>' +
                     '<input type="text" class="form-control cuttingorderID disable-typing" name="cuttingorderID[]" id="cuttingorderID_' + row_id + '" value="' + response[i].intCuttingOrderHeaderID + '" readonly>' +
                     '</td>' +
@@ -215,6 +200,71 @@ function findAlreadItemAdded() {
     });
 }
 
+function cmbCuttingOrder() {
+    $.ajax({
+        url: base_url + 'Utilities/fetchCuttingOrderHeaderData',
+        type: 'post',
+        dataType: 'json',
+        success: function(response) {
+            var html = "<option value='0'>Select Cutting Order</option>";
+            for (var i = 0; i < response.length; i++) {
+
+                html += "<option value='" + response[i]['intCuttingOrderHeaderID'] + "'>" + response[i]['vcOrderName'] + "</option>";
+            }
+
+            $("#cmbCuttingOrder").html("");
+            $("#cmbCuttingOrder").append(html);
+            CalculateItemCount();
+
+        },
+        error: function(data) {}
+
+    });
+
+}
+
+function IsAlreadyIncluded() {
+    var IsAlreadyIncluded = false;
+    var CuttingOrderHeaderID = $("#cmbCuttingOrder :selected").val();
+    if ($('#itemTable tr').length > 1) {
+        $("#itemTable tbody tr").each(function() {
+            if (CuttingOrderHeaderID == $(this).closest("tr").find('.cuttingorderID').val()) {
+                IsAlreadyIncluded = true;
+                return false;
+            }
+        });
+    }
+    if (IsAlreadyIncluded) {
+        toastr["error"]("Already Included !");
+    }
+    return (IsAlreadyIncluded == true) ? true : false;
+}
+
+
+function chkAlreadyDispatched() {
+    var ItemID = $("#cmbItem :selected").val();
+    $.ajax({
+        url: base_url + 'Utilities/chkAlreadyDispatched/' + ItemID,
+        type: 'post',
+        dataType: 'json',
+        success: function(response) {
+            var html = "<option value='0'>Select Cutting Order</option>";
+            for (var i = 0; i < response.length; i++) {
+
+                html += "<option value='" + response[i]['intCuttingOrderHeaderID'] + "'>" + response[i]['vcOrderName'] + "</option>";
+            }
+
+            $("#cmbCuttingOrder").html("");
+            $("#cmbCuttingOrder").append(html);
+            CalculateItemCount();
+
+        },
+        error: function(data) {}
+
+    });
+
+}
+
 function remove() {
     $(".red").click(function() {
 
@@ -239,27 +289,26 @@ function remove() {
 
         }
         CalculateItemCount();
-        cmbItemEnable();
+
     });
 }
 
-function cmbItemDisable() {
-    var rowCount = $('#itemTable tr').length;
-    if (rowCount > 1) {
-        document.getElementById("cmbItem").disabled = true;
-    }
-    // if (rowCount == 1) {
-    //     document.getElementById("cmbItem").disabled = false;
-    // }
-}
+// function cmbItemDisable() {
+//     var rowCount = $('#itemTable tr').length;
+//     if (rowCount > 1) {
+//         document.getElementById("cmbItem").disabled = true;
+//     }
+//     // if (rowCount == 1) {
+//     //     document.getElementById("cmbItem").disabled = false;
+//     // }
+// }
 
-function cmbItemEnable() {
-    debugger;
-    var rowCount = $('#itemTable tr').length;
-    if (rowCount == 2) {
-        document.getElementById("cmbItem").disabled = false;
-    }
-}
+// function cmbItemEnable() {
+//     var rowCount = $('#itemTable tr').length;
+//     if (rowCount == 2) {
+//         document.getElementById("cmbItem").disabled = false;
+//     }
+// }
 
 function CalculateItemCount() {
     var rowCount = $('#itemTable tr').length;
