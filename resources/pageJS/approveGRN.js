ea4317044f3107c3d7c85545071bf8606018f873
Viraj Lasitha <47885958+VirajLasitha@users.noreply.github.com>
2020-12-02 17:35:03 +0530

    Update approveGRN.js

--- resources/pageJS/approveGRN.js
+++ resources/pageJS/approveGRN.js
@@ -8,35 +8,51 @@ function CalculateItemCount() {
     $("#itemCount").text("Item Count : " + (rowCount - 1));
 }
 
+var GRN = function () {
+    this.intGRNHeaderID = 0;
+}
 
 function approveGRN(GRNHeaderID) {
     arcadiaConfirmAlert("You want to be able to approve this !", function (button) {
 
-        $.ajax({
-            async: true,
-            url: base_url + 'GRN/ApproveGRN',
-            type: 'post',
-            data: {
-                intGRNHeaderID: GRNHeaderID
-            },
-            dataType: 'json',
-            success: function (response) {
-                if (response.success == true) {
-                    arcadiaSuccessMessage("Approved !", "GRN/ViewGRN");
-                } else {
-                    toastr["error"](response.messages);
-                }
-            },
-            error: function (request, status, error) {
-                arcadiaErrorMessage(error);
+        // $.ajax({
+        //     async: true,
+        //     url: base_url + 'GRN/ApproveGRN',
+        //     type: 'post',
+        //     data: {
+        //         intGRNHeaderID: GRNHeaderID
+        //     },
+        //     dataType: 'json',
+        //     success: function (response) {
+        //         if (response.success == true) {
+        //             arcadiaSuccessMessage("Approved !", "GRN/ViewGRN");
+        //         } else {
+        //             toastr["error"](response.messages);
+        //         }
+        //     },
+        //     error: function (request, status, error) {
+        //         arcadiaErrorMessage(error);
+        //     }
+        // });
+
+        var model = new GRN();
+        model.intGRNHeaderID = GRNHeaderID;
+
+        // ajaxCall('GRN/ApproveGRN', { 'model': model }, function (response) {
+        ajaxCall('GRN/ApproveGRN', model, function (response) {
+            if (response.success == true) {
+                arcadiaSuccessMessage("Approved !", "GRN/ViewGRN");
+            } else {
+                toastr["error"](response.messages);
             }
         });
+
+
     }, this);
 }
 
-function rejectGRN(GRNHeaderID){
+function rejectGRN(GRNHeaderID) {
     arcadiaConfirmAlert("You want to be able to reject this !", function (button) {
-
         $.ajax({
             async: true,
             url: base_url + 'GRN/RejectGRN',
