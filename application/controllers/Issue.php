06e655e6d5479a49c4d670a17d78a6fcb2023a1c
Ramodk <42975445+Ramodk@users.noreply.github.com>
2020-12-04 02:56:38 +0530

    Update Issue.php

--- application/controllers/Issue.php
+++ application/controllers/Issue.php
@@ -10,8 +10,9 @@ class Issue extends Admin_Controller
         $this->data['page_title'] = 'Issue';
         $this->load->model('model_supplier');
         $this->load->model('model_item');
-        $this->load->model('model_grn');
         $this->load->model('model_measureunit');
+        $this->load->model('model_customer');
+        $this->load->model('model_issue');
     }
 
     //-----------------------------------
@@ -21,17 +22,31 @@ class Issue extends Admin_Controller
     public function CreateIssue()
     {
         if (!$this->isAdmin) {
-            if (!in_array('createIssue',$this->permission)) {
+            if (!in_array('createIssue', $this->permission)) {
                 redirect('dashboard', 'refresh');
             }
         }
 
-        $supplier_data = $this->model_supplier->getSupplierData();
-        $item_data = $this->model_item->getOnlyRawItemData();
+        $customer_data = $this->model_customer->getCustomerData();
+        $item_data = $this->model_item->getOnlyFinishItemData();
 
-        $this->data['supplier_data'] = $supplier_data;
+        $this->data['customer_data'] = $customer_data;
         $this->data['item_data'] = $item_data;
 
         $this->render_template('Issue/createIssue', 'Create Issue',  $this->data);
+    }
+
+
+    public function SaveIssue()
+    {
+        if (!$this->isAdmin) {
+            if (!in_array('createIssue', $this->permission)) {
+                redirect('dashboard', 'refresh');
+            }
+        }
+
+        $response = $this->model_issue->saveIssue();
+
+        echo json_encode($response);
     }
 }
