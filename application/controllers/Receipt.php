9602f8a90aeaa0fe4d4550e4b13603c5bfc6e62d
Ramodk <42975445+Ramodk@users.noreply.github.com>
2021-01-09 12:19:37 +0530

    Create Receipt.php

---
+++ application/controllers/Receipt.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Receipt extends Admin_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->not_logged_in();
+        $this->data['page_title'] = 'Receipt';
+        $this->load->model('model_receipt');
+        $this->load->model('model_groups');
+        $this->load->model('model_customer');
+        $this->load->model('model_issue');
+
+        $user_group_data = $this->model_groups->getUserGroupData();
+        $this->data['user_groups_data'] = $user_group_data;
+    }
+
+  //-----------------------------------
+  // Create Receipt
+  //-----------------------------------
+
+  public function CreateReceipt()
+  {
+    if (!$this->isAdmin) {
+      if (!in_array('createReceipt', $this->permission)) {
+        redirect('dashboard', 'refresh');
+      }
+    }
+
+    $customer_data = $this->model_customer->getCustomerData();
+    $payment_data = $this->model_issue->getPaymentTypes();
+    $this->data['payment_data'] = $payment_data;
+    $this->data['customer_data'] = $customer_data;
+
+    $this->render_template('Receipt/CreateReceipt', 'Create Receipt',  $this->data);
+  }
+
+
+}
