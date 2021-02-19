eac12ad12b570b10ca82c2b9f8e8efd194a4e750
Viraj Lasitha <47885958+VirajLasitha@users.noreply.github.com>
2021-02-19 22:48:35 +0530

    Update Issue.php

--- application/controllers/Issue.php
+++ application/controllers/Issue.php
@@ -81,83 +81,450 @@ class Issue extends Admin_Controller
       $issue_Header_Date =  $this->model_issue->GetIssueHeaderData($intIssueHeaderID);
       $issue_Detail_Date =  $this->model_issue->GetIssueDetailsData($intIssueHeaderID);
 
-      $html = '
+    //   $html = '
 
-            <div id="myDiv" class="wrapper">
-            <section class="invoice">
-              <!-- title row -->
-              <div class="row">
-                <div class="col-xs-12">
-                  <h2 class="page-header">
-                    "KNC Cake Boards"
-                  </h2>
-                </div>
-                <!-- /.col -->
-              </div>
-              <!-- info row -->
-              <div class="row invoice-info">
+    //         <div id="myDiv" class="wrapper">
+    //         <section class="invoice">
+    //           <!-- title row -->
+    //           <div class="row">
+    //             <div class="col-xs-12">
+    //               <h2 class="page-header">
+    //                 "KNC Cake Boards"
+    //               </h2>
+    //             </div>
+    //             <!-- /.col -->
+    //           </div>
+    //           <!-- info row -->
+    //           <div class="row invoice-info">
                 
-                <div class="col-sm-4 invoice-col">
+    //             <div class="col-sm-4 invoice-col">
 
-                  <b>Date:</b> ' . $issue_Header_Date['dtCreatedDate'] . '<br>
-                  <b>Issue No:</b> ' . $issue_Header_Date['vcIssueNo'] . '<br>
-                  <b>Customer Name:</b> ' . $issue_Header_Date['vcCustomerName'] . '<br>
+    //               <b>Date:</b> ' . $issue_Header_Date['dtCreatedDate'] . '<br>
+    //               <b>Issue No:</b> ' . $issue_Header_Date['vcIssueNo'] . '<br>
+    //               <b>Customer Name:</b> ' . $issue_Header_Date['vcCustomerName'] . '<br>
              
-                </div>
-                <!-- /.col -->
-              </div>
-              <!-- /.row -->
+    //             </div>
+    //             <!-- /.col -->
+    //           </div>
+    //           <!-- /.row -->
 
-              <!-- Table row -->
-              <div class="row">
-                <div class="col-xs-12 table-responsive">
-                  <table class="table table-striped">
-                    <thead>
-                    <tr>
-                      <th>Item name</th>
-                      <th>Unit Price</th>
-                      <th>Qty</th>
-                      <th>Amount</th>
-                    </tr>
-                    </thead>
-                    <tbody>';
+    //           <!-- Table row -->
+    //           <div class="row">
+    //             <div class="col-xs-12 table-responsive">
+    //               <table class="table table-striped">
+    //                 <thead>
+    //                 <tr>
+    //                   <th>Item name</th>
+    //                   <th>Unit Price</th>
+    //                   <th>Qty</th>
+    //                   <th>Amount</th>
+    //                 </tr>
+    //                 </thead>
+    //                 <tbody>';
+ 
+    //   foreach ($issue_Detail_Date as $k => $v) {
 
-      foreach ($issue_Detail_Date as $k => $v) {
+    //     $html .= '<tr>
+    //                       <td>' . $v['vcItemName'] . '</td>
+    //                       <td>' . $v['decUnitPrice'] . '</td>
+    //                       <td>' . $v['decIssueQty'] . '</td>
+    //                       <td>' . $v['decTotalPrice'] . '</td>
+    //                     </tr>';
+    //   }
 
-        $html .= '<tr>
-                          <td>' . $v['vcItemName'] . '</td>
-                          <td>' . $v['decUnitPrice'] . '</td>
-                          <td>' . $v['decIssueQty'] . '</td>
-                          <td>' . $v['decTotalPrice'] . '</td>
-                        </tr>';
+    //   $html .= ' <tr align="right">
+		// 	              <th>Payment Mode:</th>
+		// 	              <td>' . $issue_Header_Date['vcPaymentType'] . '</td>
+		// 	            </tr>
+		// 	            <tr align="right">
+		// 	              <th>Sub Total:</th>
+		// 	              <td>' . $issue_Header_Date['decSubTotal'] . '</td>
+		// 	            </tr>
+		// 	            <tr align="right">
+		// 	              <th>Discount:</th>
+		// 	              <td>' . $issue_Header_Date['decDiscount'] . '</td>
+    //               </tr>
+    //               <tr align="right">
+    //               <th>Grand Total:</th>
+    //               <td>' . $issue_Header_Date['decGrandTotal'] . '</td>
+    //             </tr>';
+
+    //   $html .= '  </table>
+    //       </div>
+    //     </div>
+    //     <!-- /.col -->
+    //   </div>
+    //   <!-- /.row -->
+    // </section>
+    // <!-- /.content -->
+    // </div>';
+
+    $html = '
+
+  
+
+      <style>
+      .f{
+        color: #212e59;
+        font-size:3em;
+        position: absolute;
+        font-family: "Oswald",sans-serif;
       }
+      img{
+        height: 150px;
+        position:absolute;
+        right: 30px;
+        margin-top: 40px;
+      }
+      
+      .address{
+        margin-top: 120px;
+        font-size: 13px;
+        font-family: "oswald", sans-serif;
+      }
+      .shipping-info-head{
+        column-count: 4;
+      }
+      
+      .shipping-info-head h6{
+        font-size: 20px;
+        margin-top: 0px;
+        font-family: "Teko", sans-serif; 
+      }
+      .shipping-info-head p{
+        font-size: 15px;
+        margin-top: 5px !important;
+        font-weight: 100;
+        font-family: "Teko", sans-serif; 
+      }
+      .shipping-info-1{
+        column-count: 4;
+        margin-top: -40px;
+      }
+      
+      .shipping-info-1 h6{
+        font-size: 20px;
+        margin-top: 0px;
+        font-family: "Teko", sans-serif; 
+      }
+      .shipping-info-1 p{
+        font-size: 15px;
+        margin-top: 5px !important;
+        font-weight: 100;
+        font-family: "Teko", sans-serif; 
+      }
+      .shipping-info-2{
+        column-count: 4;
+        margin-top: -27px;
+      }
+      
+      .shipping-info-2 h6{
+        font-size: 20px;
+        margin-top: 0px;
+        font-family: "Teko", sans-serif; 
+      }
+      .shipping-info-2 p{
+        font-size: 15px;
+        margin-top: 5px !important;
+        font-weight: 100;
+        font-family: "Teko", sans-serif; 
+      }
+      .shipping-info-3{
+        column-count: 4;
+        margin-top: -27px;
+      }
+      
+      .shipping-info-3 h6{
+        font-size: 20px;
+        margin-top: 0px;
+        font-family: "Teko", sans-serif; 
+      }
+      .shipping-info-3 p{
+        font-size: 15px;
+        margin-top: 5px !important;
+        font-weight: 100;
+        font-family: "Teko", sans-serif; 
+      }
+      
+      .main-strip{
+        column-count: 4;
+        margin-top: 17px;
+      }
+      
+      .main-strip h6{
+        font-size: 25px;
+        color: #212e59;
+        margin-top: 0px;
+        font-family: "Teko", sans-serif; 
+        text-align: center;
+      }
+      .top{
+        margin-bottom: -15px;
+        border-top: 2px solid red;
+      }
+      .bottom{
+        margin-top: -60px;
+        border-top: 2px solid red;
+      }
+      
+      .item-qty{
+      text-align: center !important;
+      }
+      
+      .text-right{
+        text-align: right;
+      }
+      
+      .shipping-1{
+        column-count: 4;
+        margin-top: 20px;
+      }
+      
+      .shipping-1 p{
+        font-size: 17px;
+        margin-top: 5px !important;
+        font-weight: 100;
+        font-family: "Teko", sans-serif; 
+      }
+      .shipping-2{
+        column-count: 4;
+        margin-top: -10px;
+      }
+      
+      
+      .shipping-2 p{
+        font-size: 17px;
+        margin-top: 5px !important;
+        font-weight: 100;
+        font-family: "Teko", sans-serif; 
+      }
+      .shipping-3{
+        column-count: 4;
+        margin-top: -10px;
+      }
+      
+      .shipping-3 p{
+        font-size: 17px;
+        margin-top: 5px !important;
+        font-weight: 100;
+        font-family: "Teko", sans-serif; 
+      }
+      
+      total{
+        right: 0;
+        width: 300px;
+        position: absolute;
+        margin-top: 20px;
+        margin-right: 10px;
+      
+      }
+      
+      .shipping-total{
+        column-count: 2;
+        margin-top: 10px;
+      }
+      
+      
+      .shipping-total p{
+        font-size: 17px;
+        margin-top: 5px !important;
+        font-weight: 100;
+        font-family: "Teko", sans-serif; 
+      }
+      .shipping-total-1{
+        column-count: 2;
+        margin-top: -10px;
+      }
+      
+      
+      .shipping-total-1 p{
+        font-size: 17px;
+        margin-top: 5px !important;
+        font-weight: 100;
+        font-family: "Teko", sans-serif; 
+      }
+      .shipping-total-2{
+        column-count: 2;
+        margin-top: -10px;
+      }
+      .shipping-total-2 h6{
+        font-size: 23px;
+        color: #212e59;
+        margin-top: 0px;
+        font-family: "Teko", sans-serif; 
+      }
+      
+      
+      .theTitle{
+          display: flex;
+          align-items: center;
+          height: 120px;
+          margin-top: 700px;
+          position: absolute;
+          left: 50%;
+          top: 10%;
+          transform: translate(-50%, -50%);
+          width: 600px;
+      }
+      .right, .left{
+          width: 50%;
+          overflow: hidden;
+      }
+      .theTitle h1{
+          margin: 0;
+          height: 80px;
+        margin-top: 50px;
+          line-height: 80px;
+          vertical-align: middle;
+          font-family: "Dancing Script", cursive;
+          font-weight: 600;
+          margin-left: 50px;
+          font-size: 3.5rem;
+          color: #212e59;
+          transform: translateX(100%);
+          transition: transform 0.6s ease-out .1s;
+      }
+      
+      .theTitle .separator{
+          background-color: black;
+          width: 4px;
+          transform: rotate(90deg);
+          height: 0;
+          display: block;
+          margin: 0 6px 0 0;
+          transition: height 0.3s ease 0.9s, transform 0.3s ease 0.6s;
+      }
+      
+      .active .separator{
+          height: 100%;
+          transform: rotate(0);
+          transition: height 0.3s ease, transform 0.3s ease 0.4s;
+      
+      }
+      .theTitle.active h1{
+          transform: translateX(0);
+          transition: transform 0.6s ease-out 0.8s;
+      }
+      .theTitle.active p{
+          transform: translateX(0);
+          transition: transform 0.5s ease-out 0.7s;
+      }
+      .theTitle .right{
+          display: flex;
+          align-items: center;
+          width: 50%;
+      }
+      .theTitle p{
+          margin: 0;
+          padding: 0;
+          font-family: "Teko";
+          font-size: 1.3rem;
+          font-weight: 400;
+          line-height: 200px;
+          margin-top: 0px;
+          vertical-align: middle;
+          display: flex;
+          align-items: center;
+          text-transform: uppercase;
+          color: red;
+          transform: translateX(-100%);
+          transition: transform 0.5s ease-out;
+      }
+      
+      @media(max-width: 750px){
+        img{
+          height: 70px;
+          position:absolute;
+          margin-left: 430px;
+          margin-top: 40px;
+      }
+      
+      }
+      </style>
 
-      $html .= ' <tr align="right">
-			              <th>Payment Mode:</th>
-			              <td>' . $issue_Header_Date['vcPaymentType'] . '</td>
-			            </tr>
-			            <tr align="right">
-			              <th>Sub Total:</th>
-			              <td>' . $issue_Header_Date['decSubTotal'] . '</td>
-			            </tr>
-			            <tr align="right">
-			              <th>Discount:</th>
-			              <td>' . $issue_Header_Date['decDiscount'] . '</td>
-                  </tr>
-                  <tr align="right">
-                  <th>Grand Total:</th>
-                  <td>' . $issue_Header_Date['decGrandTotal'] . '</td>
-                </tr>';
-
-      $html .= '  </table>
-          </div>
+  
+      <body>
+      <!-- partial:index.partial.html -->
+      <div class="page">
+        <h1 class="f">INVOICE</h1>
+        <img src="https://img.freepik.com/free-vector/illustration-circle-stamp-banner-vector_53876-27183.jpg?size=338&ext=jpg" ALIGN="right">
+        <p class="address"><b>KNC Cake Boards & Boxes</b><br>No.124A<br>Galle Road, Pohoddaramulla<br>Wadduwa</p>
+        <div class="shipping-info-head">
+          <h6>INVOICED TO</h6>
+          <h6>SHIP TO</h6>
+          <h6>INVOICE#</h6>
+          <p>INV21010001</p>
         </div>
-        <!-- /.col -->
+        <div class="shipping-info-1">
+          <p>Client Name</p>
+          <p>John Smith</p>
+          <h6>INVOICE DATE</h6>
+          <p>11/02/2019</p>
+        </div>
+        <div class="shipping-info-2">
+          <p>Address Line 1</p>
+          <p>37 Drive</p>
+          <h6>P.O.#</h6>
+          <p>2023/2019</p>
+        </div>
+        <div class="shipping-info-3">
+          <p>Address Line 2</p>
+          <p>Cambridge, MA 16543</p>
+          <h6>DUE DATE</h6>
+          <p>26/2/2019</p>
+        </div>
+        <hr class="top">
+        <div class="main-strip">
+          <h6>DESCRIPTION</h6>
+          <h6>QTY</h6>
+          <h6>UNIT PRICE</h6>
+          <h6>AMOUNT</h6>
+        </div>
+        <hr class="bottom">
+        <div class="shipping-1">
+          <p>Front and rear break cables</p>
+          <p class="item-qty">1</p>
+          <p class="text-right">$ 100.00</p>
+          <p class="text-right">$ 100.00</p>
+        </div>
+        <div class="shipping-2">
+          <p>New set of pedal arms</p>
+          <p class="item-qty">2</p>
+          <p class="text-right">$ 15.00</p>
+          <p class="text-right">$ 30.00</p>
+        </div>
+        <div class="shipping-3">
+          <p>Lollipops</p>
+          <p class="item-qty">3</p>
+          <p class="text-right">$ 5.00</p>
+          <p class="text-right">$ 15.00</p>
+        </div>
+        <hr class="top">
+        <total >
+        <div class="shipping-total">
+          <p>Subtotal</p>
+          <p class="text-right">$ 145.00</p>
+        </div>
+        <!--<div class="shipping-total-1">
+          <p>Sales Tax 6.25%</p>
+          <p class="text-right">9.06</p>
+        </div> -->
+        <div class="shipping-total-2">
+          <h6>TOTAL</h6>
+          <h6 class="text-right">$ 154.06</h6>
+        </div>
+        </total>
+        <!-- <div class="theTitle active">
+              <div class="left"><h1>Thank You</h1></div>
+              <span class="separator"></span>
+              <div class="right">
+                <p>Payment is due within 15 days.</p>
+          </div>
+          </div> -->
       </div>
-      <!-- /.row -->
-    </section>
-    <!-- /.content -->
-    </div>';
+      <!-- partial -->
+        
+      </body>';
 
       return $html;
     }
